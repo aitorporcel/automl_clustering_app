a9b4a0a7941bbee47ac8ec0824ce74175b655a43
Aitor Porcel Laburu <aitor.porcellaburu@gmail.com>
2022-09-29 23:01:20 +0200

    Added advanced options

--- app.py
+++ app.py
@@ -54,8 +54,52 @@ if uploaded_file is not None:
         [],
         help = cols_to_drop_help)
 
+    with st.expander("Advanced options"):
+        st.write("UMAP parameters")
+        c1, c2 = st.columns(2)
+        with c1:
+            n_components = st.number_input("N° components", min_value = 1, value = 2)
+            n_neighbors = st.number_input("N° neighbors", min_value = 0, value = 15)
+            scaler_type_numerical = st.selectbox("Scaler numerical",
+                                 options = ["standard", "robust"], index = 0)
+            transformation_type_numerical = st.selectbox("Transformation numerical",
+                                 options = ["power", "quantile"], index = 0)
+            
+        with c2:
+            
+            threshold_combine_rare_levels = st.number_input("Threshold categorical level", 
+                                min_value = 0.0, max_value = 1.0, value = 0.0)
+            imputer_strategy_numerical = st.selectbox("Imputation numerical",
+                                 options = ["mean", "median", "most_frequent"], index = 0)
+            umap_combine_method = st.selectbox("UMAP combine method",
+                                 options = ["intersection", "union", "contrast", "intersection_union_mapper"], index = 0)
+            n_neighbors_intersection_union = st.number_input("N° neighbors intersection", min_value = 0, value = 15)
+
+        st.write("\n")
+        st.write("HDBSCAN parameters")
+        c1, c2 = st.columns(2)
+        with c1:
+            min_cluster_size = st.number_input("Min cluster size", min_value = 0, value = 15)
+            if optimize_model:
+                n_trials = st.number_input("N° trials optimization", min_value = 0,
+                             max_value = 300, value = 100)
+
+        with c2:
+            min_samples = st.number_input("Min samples", min_value = 0, value = 15)
+    
     if st.button('Run analysis'):
-        clf = QuickClus(n_components = 2)
+        #clf = QuickClus(n_components = 2)
+        clf = QuickClus(n_neighbors = n_neighbors, min_cluster_size = min_cluster_size,
+                min_samples = min_samples,
+                threshold_combine_rare_levels = threshold_combine_rare_levels,
+                n_components = n_components,
+                scaler_type_numerical = scaler_type_numerical,
+                imputer_strategy_numerical = imputer_strategy_numerical,
+                transformation_type_numerical = transformation_type_numerical,
+                umap_combine_method = umap_combine_method,
+                n_neighbors_intersection_union = None,
+                verbose = False)
+
         if len(cols_to_drop) > 0:
             clf.fit(df.drop(cols_to_drop, axis = 1))
         results = clf.assing_results(df)
@@ -66,7 +110,7 @@ if uploaded_file is not None:
         st.pyplot(clf.plot_2d_labels())
 
         if optimize_model:
-            clf.tune_model(n_trials=100)
+            clf.tune_model(n_trials=n_trials)
             st.subheader("After optimization")
             results = clf.assing_results(df)
             st.pyplot(clf.plot_2d_labels())
